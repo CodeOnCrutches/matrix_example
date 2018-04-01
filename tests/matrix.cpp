a1d782f97a2b045f4dea6045b6f6894332081469
Dzhabrail Elmezov <elmezov007@gmail.com>
2018-04-01 14:39:19 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -81,6 +81,43 @@ TEST_CASE("add, [oper+]")
     REQUIRE ( (A + B) == C);
 }
 
+TEST_CASE("add, [oper+], double")
+{
+    std::string input{
+        "3, 3\n"
+        "1.5 1.5 1.5\n"
+        "1.5 1.5 1.5\n"
+        "1.5 1.5 1.5" };
+    std::string input_two
+    {
+        "3, 3\n"
+        "1.5 1.5 1.5\n"
+        "1.5 1.5 1.5\n"
+        "1.5 1.5 1.5"
+    };
+    matrix_t<double> A;
+    matrix_t<double> B;
+    std::istringstream istream{ input };
+    std::istringstream istream_two { input_two };
+    REQUIRE( A.read( istream ) );
+    REQUIRE( B.read( istream_two ) );
+    
+    std::string solution
+    {
+        "3, 3\n"
+        "3 3 3\n"
+        "3 3 3\n"
+        "3 3 3"
+    };
+    matrix_t<double> C;
+    std::istringstream add{ solution };
+    REQUIRE( C.read(add) );
+    
+    REQUIRE_NOTHROW( A.collumns() == B.collumns() && A.rows() == B.rows() );
+    
+    REQUIRE ( (A + B) == C);
+}
+
 TEST_CASE("sub, [oper-]")
 {
     std::string input{
@@ -129,10 +166,12 @@ TEST_CASE("mul, [oper*]")
         "1 1 1\n"
         "1 1 1"
     };
-    matrix_t A;
-    matrix_t B;
+    matrix_t<int> A;
+    matrix_t<int> B;
+    
     std::istringstream istream{ input };
     std::istringstream istream_two { input_two };
+    
     REQUIRE( A.read( istream ) );
     REQUIRE( B.read( istream_two ) );
     
@@ -143,12 +182,15 @@ TEST_CASE("mul, [oper*]")
         "6 6 6\n"
         "6 6 6"
     };
-    matrix_t C;
+    
+    matrix_t<int> C;
     std::istringstream add{ solution };
+    
     REQUIRE( C.read(add) );
     REQUIRE( (A * B) == C);
 }
-    
+
+
 TEST_CASE("MINUS, [oper-=]")
 {
     std::string input{
@@ -171,15 +213,20 @@ TEST_CASE("MINUS, [oper-=]")
         "1 1 1"
     };
     
-    matrix_t A;
-    matrix_t B;
-    matrix_t C;
+    matrix_t<int> A;
+    matrix_t<int> B;
+    matrix_t<int> C;
+    
     std::istringstream istream_res { res_input };
     std::istringstream istream { input };
     std::istringstream istream_two { input_two };
+    
     REQUIRE( C.read( istream_res ) );
     REQUIRE( A.read( istream ) );
     REQUIRE( B.read( istream_two ) );
+    
+    REQIURE_NOTHROW( A.collumns() == B.collumns() && A.rows() == B.rows() );
+    
     REQUIRE( (A -= B) == C );
 }
 
@@ -205,15 +252,20 @@ TEST_CASE("add, [oper+=]")
         "3 3 3"
     };
     
-    matrix_t A;
-    matrix_t B;
-    matrix_t C;
+    matrix_t<int> A;
+    matrix_t<int> B;
+    matrix_t<int> C;
+    
     std::istringstream istream_res { res_input };
     std::istringstream istream { input };
     std::istringstream istream_two { input_two };
+    
+    REQIRE_NOTHROW( A.collunms() == B.collumns() && A.rows() == B.rows() );
+    
     REQUIRE( C.read( istream_res ) );
     REQUIRE( A.read( istream ) );
     REQUIRE( B.read( istream_two ) );
+    
     REQUIRE( ( A += B ) == C );
 }
 
@@ -242,14 +294,16 @@ TEST_CASE("multi, [oper*=]")
         "6 6 6"
     };
     
-    matrix_t A;
-    matrix_t B;
-    matrix_t C;
+    matrix_t<int> A;
+    matrix_t<int> B;
+    matrix_t<int> C;
     
     std::istringstream istream_A { input_A };
     std::istringstream istream_B { input_B };
     std::istringstream istream_C { input_C };
    
+    REQUIRE_NOTHROW( A.collumns() == B.rows() );
+    
     REQUIRE( A.read( istream_A ) );
     REQUIRE( B.read( istream_B ) ); 
     REQUIRE( C.read( istream_C ) );
